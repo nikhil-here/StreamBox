48991dfdca668fae10e2ef00a640fd9ab0771150
nikhil-here <nikhilmandlik43@gmail.com>
2025-03-25 00:09:29 +0530

    MediaSource base and CameraSource Implementation
    - Writing base Implementation for MediaSource
    - Writing CameraSource Implementation
    - Creating Demo MainScreen to utilize CameraSource

--- streambox/build.gradle.kts
+++ streambox/build.gradle.kts
@@ -40,4 +40,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.video)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.extensions)
 }
